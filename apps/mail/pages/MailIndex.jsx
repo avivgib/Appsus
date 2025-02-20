ac4d9edb176be4a66fc3c1413fdd69896b907aed
eliad <eliad.haim@gmail.com>
2025-02-21 00:21:59 +0200

    Add load emails

--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -1,5 +1,26 @@
+import { mailService } from '../services/mail.service.js'
+const { useState, useEffect, useRef } = React
 
 export function MailIndex() {
-    return <section className="container">Mail app</section>
+
+    const [emails, setEamils] = useState(null)
+    console.log(emails);
+
+    useEffect(() => {
+        loadEmails()
+    }, [])
+
+    function loadEmails() {
+        mailService.query()
+            .then(emails => setEamils(emails))
+            .catch(error => console.error(error))
+    }
+
+    if (!emails) return 'loading...'
+    return (
+        <section className="container">
+            Emails
+        </section>
+    )
 }
 
