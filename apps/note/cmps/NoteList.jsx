f261052dce157af212ef3f37086805812afab12a
Aviv Gibli <avivgib1012@gmail.com>
2025-02-23 16:40:12 +0200

    Implement note preview, list, and delete

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -1,3 +1,21 @@
-export function NoteList() {
-    return <div>note list</div>
+import { NotePreview } from "./NotePreview.jsx"
+
+export function NoteList({notes, onRemoveNote}) {
+    return (
+        <section className='note-list'>
+            {notes.map(note => (
+                <article key={note.id} className='note-card'>
+
+                    <NotePreview note={note} />
+
+                    {/* add edit modal by clicking on note-options class */}
+                    <section className="note-options"> 
+                        <button onClick={() => onRemoveNote(note.id)}>Delete note</button>
+                    </section>
+
+                </article>
+            ))}
+        </section>
+
+    )
 }
--- apps/note/pages/NoteIndex.jsx
+++ apps/note/pages/NoteIndex.jsx
@@ -1,4 +1,44 @@
+import { showErrorMsg, showSuccessMsg } from "../../../services/event-bus.service.js"
+import { noteService } from "../services/note.service.js"
+import { NoteList } from "../cmps/NoteList.jsx"
+
+const { useState, useEffect, useRef } = React
 
 export function NoteIndex() {
-    return <section className="container">Notes app</section>
+    const [notes, setNotes] = useState(() => [])
+
+    useEffect(() => {
+        console.log('Fetching notes...')
+        loadNotes()
+    }, [])
+
+
+    function loadNotes() {
+        noteService.query()
+            .then(prevNotes => {
+                console.log('Notes from service: ', prevNotes)
+                setNotes(prevNotes)
+            })
+            .catch(err => {
+                console.error('Error loading notes', err)
+                setNotes([])
+            })
+    }
+
+    function onRemoveNote(noteId) {
+        noteService.remove(noteId)
+            .then(() => {
+                setNotes(prevNotes => prevNotes.filter((note) => note.id !== noteId))
+                showSuccessMsg('Note trashed')
+            })
+            .catch(() => showErrorMsg('Note unpinned and trashed'))
+    }
+
+    return (
+        <section className="container">
+            <h1>Notes app</h1>
+            <NoteList notes={notes} onRemoveNote={onRemoveNote} />
+
+        </section>
+    )
 }
