92de4764398bee8c00c53b321b911babca30c2bd
eliad <eliad.haim@gmail.com>
2025-02-24 21:32:44 +0200

    Add email not found line

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -8,22 +8,26 @@ export function MailList({ onSetcmpType, emails, onOpenMailDetails, onToggleIsRe
             <div className='emails-bar flex align-center'>
             </div>
             <ul className='emails-list clean-list'>
-                {emails.map(mail => {
-                    return <li key={mail.id} className={`mail-preview ${mail.isRead ? '' : 'unread'}`}
-                        onClick={() => { onOpenMailDetails(mail.id); onSetcmpType('details') }}>
-                        <div className='grip'><span className='fa grip-vertical'></span></div>
-                        <div className='mail-envelope'>
-                            <span className={`fare ${mail.isRead ? 'envelope-open' : 'envelope'}`}
-                                onClick={(event) => onToggleIsRead(event, mail.id)}></span>
-                        </div>
-                        <div className='mail-remove'>
-                            <span className='fare trash-can' onClick={(event) => { onRemoveMail(event, mail.id) }}></span>
-                        </div>
-                        <div className='mail-star'><span className='fare star'></span></div>
-                        <MailPreview mail={mail} />
-                    </li>
-                })}
-
+                {emails.length > 0
+                    ? (
+                        emails.map(mail => {
+                            return <li key={mail.id} className={`mail-preview ${mail.isRead ? '' : 'unread'}`}
+                                onClick={() => { onOpenMailDetails(mail.id); onSetcmpType('details') }}>
+                                <div className='grip'><span className='fa grip-vertical'></span></div>
+                                <div className='mail-envelope'>
+                                    <span className={`fare ${mail.isRead ? 'envelope-open' : 'envelope'}`}
+                                        onClick={(event) => onToggleIsRead(event, mail.id)}></span>
+                                </div>
+                                <div className='mail-remove'>
+                                    <span className='fare trash-can' onClick={(event) => { onRemoveMail(event, mail.id) }}></span>
+                                </div>
+                                <div className='mail-star'><span className='fare star'></span></div>
+                                <MailPreview mail={mail} />
+                            </li>
+                        })
+                    )
+                    : <li className='no-mail'>No emails found</li>
+                }
             </ul >
         </section>
     )
