bd766a92d21cf27f063bd951bc503c85b4bcc4f3
eliad <eliad.haim@gmail.com>
2025-02-25 19:10:25 +0200

    Add sort style

--- apps/mail/cmps/MailSort.jsx
+++ apps/mail/cmps/MailSort.jsx
@@ -3,10 +3,14 @@ const { useState, useEffect, useRef } = React
 export function MailSort({ sortBy, onSetSortBy, filterBy }) {
 
     const [editSortBy, setEditSortBy] = useState({ ...sortBy })
+    const [ischecked, setIschecked] = useState(-1)
 
     // rest the editFilterBy if filterBy get status
     useEffect(() => {
-        setEditSortBy(prev => ({ ...sortBy }))
+        if (filterBy.status !== '') {
+            setIschecked(-1)
+            setEditSortBy(prev => ({ ...sortBy }))
+        }
     }, [filterBy.status])
 
     useEffect(() => {
@@ -18,6 +22,7 @@ export function MailSort({ sortBy, onSetSortBy, filterBy }) {
         var { value, type, name } = ev.target
 
         value = (value === '') ? '' : +value
+        setIschecked(value)
 
         setEditSortBy(prev => ({ [name]: value }))
     }
@@ -25,17 +30,16 @@ export function MailSort({ sortBy, onSetSortBy, filterBy }) {
     const { title, date } = editSortBy
     return (
         <section className='mail-sort flex align-center '>
-            <select name="title" id='title' value={title || ''} onChange={onSetEditSortBy} >
+            <select name="title" id='title' className={title ? 'selected' : ''} value={title || ''} onChange={onSetEditSortBy} >
                 <option value=''>By Title</option>
-                <option value={1}>A to Z</option>
-                <option value={-1}>Z to A</option>
+                <option value={1}>{ischecked === 1 && title ? ' ✓ ' : ''} A to Z</option>
+                <option value={-1}>{ischecked === -1 && title ? ' ✓ ' : ''} Z to A</option>
             </select>
 
-            <select name="date" id='date' value={date || ''} onChange={onSetEditSortBy} >
+            <select name="date" id='date' className={date ? 'selected' : ''} value={date || ''} onChange={onSetEditSortBy} >
                 <option value=''>By Date</option>
-                <option value={-1}>Newest First</option>
-                <option value={1}>Oldest First</option>
-
+                <option value={-1}>{ischecked === -1 && date ? ' ✓ ' : ''}Newest First</option>
+                <option value={1}>{ischecked === 1 && date ? ' ✓ ' : ''}Oldest First</option>
             </select>
         </section>
     )
