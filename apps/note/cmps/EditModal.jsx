e0e38c298dc37ae4bd863ca3c27a1dec09ba2ff3
Aviv Gibli <avivgib1012@gmail.com>
2025-02-26 12:20:51 +0200

    Enhance EditModal with dynamic textarea resizing and improved styling

--- apps/note/cmps/EditModal.jsx
+++ apps/note/cmps/EditModal.jsx
@@ -1,7 +1,8 @@
-const { useState, useEffect } = React
+const { useState, useEffect, useRef } = React
 
 export function EditModal({ note, onClose, onSave }) {
     const [editedNote, setEditedNote] = useState(note)
+    const textareaRef = useRef(null)
 
     useEffect(() => {
         setEditedNote(note)
@@ -15,23 +16,36 @@ export function EditModal({ note, onClose, onSave }) {
     }
 
     function handleClose() {
-        const notesAreDifferent = JSON.stringify(editedNote) !== JSON.stringify(note);
+        const notesAreDifferent = JSON.stringify(editedNote) !== JSON.stringify(note)
 
         if (notesAreDifferent) {
             onSave(editedNote)
-            setTimeout(onClose, 1500);
+            setTimeout(onClose, 1500)
         } else {
             onClose()
         }
     }
 
+    function handleContentSize(event) {
+        event.target.style.height = "auto";
+        event.target.style.height = `${event.target.scrollHeight}px`;
+    }
+
     return (
         <div className="modal-overlay show" onClick={handleClose}>
             <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-                
+
                 <div className="modal-body">
                     <input type="text" name="title" value={editedNote.info.title} onChange={handleChange} />
-                    <textarea name="content" rows={13} value={editedNote.info.content} onChange={handleChange}></textarea>
+
+                    <textarea
+                        ref={textareaRef}
+                        name="content"
+                        value={editedNote.info.content}
+                        onChange={handleChange}
+                        onInput={handleContentSize}
+                    />
+
                 </div>
 
                 <div className="modal-footer">
