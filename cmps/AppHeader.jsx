ae9f374169fc3831e7f03db0116c3ad3e4a24cf0
eliad <eliad.haim@gmail.com>
2025-03-02 22:10:09 +0200

    Add DynamicLogo

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,11 +1,28 @@
-const { Link, NavLink } = ReactRouterDOM
+const { Link, NavLink, useLocation } = ReactRouterDOM
+const { useState, useEffect, useRef } = React
 
 export function AppHeader() {
 
+    const [currPage, setCurrPage] = useState(null)
+    const location = useLocation()
+
+    useEffect(() => {
+        if (location.pathname) {
+            onSetCurrPage(location.pathname)
+        }
+    }, [location.pathname])
+
+    function onSetCurrPage(pathname) {
+        setCurrPage(pathname)
+    }
+
     return <header className="app-header">
-        <Link to="/">
-            <h3>LOGO ✨</h3>
+        <Link to={currPage || '/'}>
+            <div className='main-logo'>
+                <DynamicLogo currPage={currPage} />
+            </div>
         </Link>
+
         <nav>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/about">About</NavLink>
@@ -14,3 +31,24 @@ export function AppHeader() {
         </nav>
     </header>
 }
+
+
+function DynamicLogo({ currPage }) {
+    switch (currPage) {
+        case '/':
+            return <div className='page-name'>Apsus</div>
+        case '/about':
+            return <div className='page-name'>About</div>
+        case '/mail':
+            return <React.Fragment>
+                <img src="assets/images/gmail-logo.png" className='logo-img' alt="gmail" />
+                <div className='page-name'>Gmail</div>
+            </React.Fragment>
+        case '/note':
+            return <React.Fragment>
+                <img src="assets/images/keep-logo.png" className='logo-img' alt="gmail" />
+                <div className='page-name'>Keep</div>
+            </React.Fragment>
+    }
+}
+
