f6deaa3d16d76a322bb2c16b004e43188e644853
eliad <eliad.haim@gmail.com>
2025-03-05 03:52:10 +0200

    Add about page

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,6 +1,38 @@
 
 export function About() {
-    return <section className="container about">
-        <h1>About Page</h1>
-    </section>
+    return (
+        <section className="about">
+            <h1>About Us</h1>
+
+            <div className='creator-box aviv'>
+                <div className='creator-img'>
+                    <img src="assets/images/aviv.jpg" alt="" />
+                </div>
+                <div className='creator-info'>
+                    <div className='name'>
+                        Aviv
+                    </div>
+                    <p className='about-me'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, ut, aliquam consectetur maxime dicta eum laudantium iusto quam eaque nostrum ratione mollitia quasi repellat! Sunt incidunt sit rem distinctio ratione.
+                    </p>
+                </div>
+            </div>
+
+            <div className='creator-box eliad'>
+                <div className='creator-img'>
+                    <img src="assets/images/eliad.jpeg" alt="" />
+                </div>
+                <div className='creator-info'>
+                    <div className='name'>
+                        Eliad
+                    </div>
+                    <p className='about-me'>
+                        Hello, my name is Eliad. I'm 30 years old and currently learning programming.
+                    </p>
+                </div>
+            </div>
+
+
+        </section>
+    )
 }
